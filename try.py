3c3640ddc2eed11e8085a457e5e3e6b8933733b6
anshnigam <anshnigam05@gmail.com>
2023-03-03 22:19:11 +0530

    integrated frontend to ml model

--- try.py
+++ try.py
@@ -1,4 +1,13 @@
-import pickle
 import pandas as pd
-md = pickle.load(open('model.pkl', 'rb'))
-print(md.predict([[1,0,1,26,0,0,0,1,0,1,0,1,0,3,5,30,0,1,4,6,8]]))
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('diabetes.csv')
+x = df.iloc[:,0:-1]
+y = df.iloc[:,-1]
+x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=0,test_size=0.25)
+
+model = pickle.load(open('model.pkl', 'rb'))
+y_pred = model.predict(x_test)
+print(accuracy_score(y_pred,y_test))
